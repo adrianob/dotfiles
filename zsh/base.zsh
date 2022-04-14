94119ff2fa440c9c9104301099595c088b8b569a
Adriano Benin <adriano.benin@gmail.com>
2022-04-13 17:18:50 -0700

    add package config

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -4,7 +4,7 @@ include() {
 }
 
 export DOTFILES="$HOME/dotfiles"
-
+export DEV_PACKAGES="zsh"
 
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
